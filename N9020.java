712b4f17946a1f83567deb8b49d88d2f6ce79c4c
yjeom <44234192+yjeom@users.noreply.github.com>
2021-08-05 20:47:33 +0900

    골드바흐의 추측(2)
    https://www.acmicpc.net/problem/9020

--- N9020.java
+++ N9020.java
@@ -3,24 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class N9020 {
-    public static int findMaxPrime(int n){
-        int max=2;
-        for(int i=n;i>=2;i--){
-            boolean isPrime=true;
-            for(int j=2;j<i;j++){
-                if(i%j==0){
-                    isPrime=false;
-                    break;
-                }
-            }
-            if(isPrime){
-                if(i>max){
-                    max=i;
-                    break;
-                }
+    public static boolean isPrime(int n){
+        boolean isPrime=true;
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                isPrime=false;
+                break;
             }
         }
-        return max;
+        return isPrime;
     }
     public static void main(String[]args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -31,14 +22,11 @@ public class N9020 {
             inputs[i]=Integer.parseInt(br.readLine());
         }
         for(int i=0;i<inputs.length;i++){
-            int j=0;
-            int a=findMaxPrime((inputs[i]/2)-j);
-            int b=findMaxPrime(inputs[i]-((inputs[i]/2)-j));
-            while(a+b!=inputs[i]){
-                j++;
-                a=findMaxPrime((inputs[i]/2)-j);
-                b=findMaxPrime(inputs[i]-((inputs[i]/2)-j));
-
+            int a=inputs[i]/2;
+            int b=inputs[i]-a;
+            while(!isPrime(a)||!isPrime(b)){
+                a--;
+                b++;
             }
             System.out.println(a+" "+b);
         }
