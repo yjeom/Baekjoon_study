10ca2ac24fd3ffbf3e8249abda2e07d01b8f7b25
yjeom <44234192+yjeom@users.noreply.github.com>
2021-08-17 14:52:51 +0900

    영화감독 숌
    https://www.acmicpc.net/problem/1436

---
+++ N1436.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N1436 {
+    public static void main(String[]args)throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        int count=1;
+        int num=666;
+        while (count !=N){
+            num++;
+            if(String.valueOf(num).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(num);
+
+    }
+}
