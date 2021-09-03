3228cafc4ba1e44bc9052ace1dfcd1b41b8c8a49
yjeom <44234192+yjeom@users.noreply.github.com>
2021-09-03 20:10:05 +0900

    그대로 출력하기
    https://www.acmicpc.net/problem/11718

---
+++ N11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class N11718 {
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        while(scan.hasNext()){
+            String input= scan.nextLine();
+            System.out.println(input);
+        }
+
+
+    }
+}
