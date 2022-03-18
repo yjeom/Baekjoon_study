699a2c89cad0066720576f2b4d744644ee4e5b3c
yjeom <44234192+yjeom@users.noreply.github.com>
2022-03-18 17:22:45 +0900

    에디터
    https://www.acmicpc.net/problem/1406

---
+++ N1406.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Iterator;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class N1406 {
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Character> left=new Stack<>();
+        Stack<Character> right=new Stack<>();
+        char[] inputs=br.readLine().toCharArray();
+        for(int i=0;i<inputs.length;i++){
+            left.push(inputs[i]);
+        }
+        int n=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            String command= st.nextToken();
+            if(command.equals("L")){
+                if(!left.empty()){
+                    char temp=left.pop();
+                    right.push(temp);
+                }
+            }else if(command.equals("D")){
+                if(!right.empty()){
+                    char temp=right.pop();
+                    left.push(temp);
+                }
+            }else if(command.equals("B")){
+                if(!left.empty()){
+                    left.pop();
+                }
+            }else if(command.equals("P")){
+                left.push(st.nextToken().charAt(0));
+            }
+        }
+        Iterator<Character> iter=left.iterator();
+        while(iter.hasNext()){
+            bw.write(iter.next());
+        }
+        while(!right.empty()){
+            bw.write(right.pop());
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
