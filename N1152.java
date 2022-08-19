a0eb7867f590b22b30d5a93167415c443860a82c
yjeom <44234192+yjeom@users.noreply.github.com>
2022-08-19 16:06:58 +0900

    단어의 개수
    https://www.acmicpc.net/problem/1152

---
+++ N1152.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class N1152 {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        StringTokenizer st=new StringTokenizer(scanner.nextLine());
+        System.out.println(st.countTokens());
+    }
+}
